2f0b2e26caaf41c655c1dd1da2ef3ffffab8fa72
Jefwillems <jefwillems100@gmail.com>
2019-04-23 16:54:27 +0200

    update script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 CWD="$(pwd)"
 
+
+
 pip install atramhasis
 pip install ./requirements-dev.txt
 python setup.py compile_catalog
@@ -9,8 +11,8 @@ cd atramhasis_gent/static
 bower install
 cd admin
 bower install
-cd $CWD
+cd ${CWD}
 alembic upgrade head
 python setup.py develop
 pserve development.ini
-# DB_URL="sqlite:///atramhasis_gent.sqlite" pserve development.ini
+dump_rdf development.ini
