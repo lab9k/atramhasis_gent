cb41ce6bd31f45584ac3d41ee9665c9c893addc1
Jefwillems <jefwillems100@gmail.com>
2019-04-24 14:21:28 +0200

    migrate database in start

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+alembic upgrade head
 sqlite3 atramhasis_gent.sqlite "INSERT INTO conceptscheme SELECT 1, 'http://stad.gent/id/concepts/gent_words' WHERE NOT EXISTS(SELECT 1 FROM conceptscheme WHERE id = 1);"
 pserve development.ini
