b0b49fbb17cfdaeae407e588ada08675343331ae
Jefwillems <jefwillems100@gmail.com>
2019-05-06 16:59:06 +0200

    add thesauri in skos init

--- atramhasis_gent/skos/__init__.py
+++ atramhasis_gent/skos/__init__.py
@@ -14,6 +14,22 @@ def includeme(config):
         },
         config.registry.dbmaker
     )
+    THEMES_GENT = SQLAlchemyProvider(
+        {
+           'id': 'gent_themes',
+           'conceptscheme_id': 2,
+        },
+        config.registry.dbmaker
+    )
+    INFO_TYPES = SQLAlchemyProvider(
+        {
+           'id': 'gent_themes',
+           'conceptscheme_id': 3,
+        },
+        config.registry.dbmaker
+    )
 
     skosregis = config.get_skos_registry()
     skosregis.register_provider(atram_gent)
+    skosregis.register_provider(THEMES_GENT)
+    skosregis.register_provider(INFO_TYPES)
