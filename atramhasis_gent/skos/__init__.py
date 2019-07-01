0d006818da237d058d7ded8002155203f45e2d75
Jefwillems <jefwillems100@gmail.com>
2019-07-01 14:00:29 +0200

    fetch conceptschemes dynamically

--- atramhasis_gent/skos/__init__.py
+++ atramhasis_gent/skos/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import logging
+
 from skosprovider_sqlalchemy.providers import SQLAlchemyProvider
-from skosprovider_getty.providers import AATProvider
-from skosprovider.providers import VocabularyProvider
+from sqlalchemy import create_engine
+
 from atramhasis_gent.uri.generators import UriPatternGenerator
 
 log = logging.getLogger(__name__)
@@ -22,37 +23,27 @@ def includeme(config):
 
 
 def get_internal_providers(config):
-    atram_gent = SQLAlchemyProvider(
-        {
-            'id': 'gent_words',
-            'conceptscheme_id': 1,
-        },
-        config.registry.dbmaker,
-        uri_generator=UriPatternGenerator(
-            'https://stad.gent/id/concepts/gent_words/%s'
+    print(config.get_settings()['sqlalchemy.url'])
+    ret = []
+    engine = create_engine(config.get_settings()['sqlalchemy.url'], echo=True)
+    engine.connect()
+    result = engine.execute('SELECT * FROM conceptscheme')
+    for row in result:
+        scheme_db_id = row[0]
+        scheme_uri = row[1]
+        scheme_id = scheme_uri.split('/')[-1]
+        print(f'id: {scheme_db_id}, uri: {scheme_uri}, scheme_id: {scheme_id}')
+        scheme = SQLAlchemyProvider(
+            {
+                'id': scheme_id,
+                'conceptscheme_id': scheme_db_id
+            },
+            config.registry.dbmaker,
+            uri_generator=UriPatternGenerator(f'{scheme_uri}/%s')
         )
-    )
-    themes_gent = SQLAlchemyProvider(
-        {
-            'id': 'gent_themes',
-            'conceptscheme_id': 2,
-        },
-        config.registry.dbmaker,
-        uri_generator=UriPatternGenerator(
-            'https://stad.gent/id/concepts/gent_themes/%s'
-        )
-    )
-    info_types = SQLAlchemyProvider(
-        {
-            'id': 'gent_info_types',
-            'conceptscheme_id': 3,
-        },
-        config.registry.dbmaker,
-        uri_generator=UriPatternGenerator(
-            'https://stad.gent/id/concepts/gent_info_types/%s'
-        )
-    )
-    return [atram_gent, themes_gent, info_types, ]
+        ret.append(scheme)
+    engine.dispose()
+    return ret
 
 
 def get_external_providers():
